5ae5e33aaadbb50d7d05e97535023d270bfff203
Abdusalomov A'zam <azamabdusalomov@icloud.com>
2023-02-20 18:38:57 +0500

    done!

---
+++ bin/index.js
@@ -0,0 +1,97 @@
+#! /usr/bin/env node
+import path from "path";
+import fs from "fs/promises";
+import {
+    eslintRc,
+    gitIgnore,
+    nodemonJson,
+    packageJson,
+    prettierRc,
+    printHelp,
+    tsConfig,
+} from "../utils.js";
+
+if (process.argv[2] == "new") {
+    const projectName = process.argv[3];
+    const projectFolder = path.join(process.cwd(), projectName);
+
+    // Proyekt papkasini yaratish!
+    fs.mkdir(projectFolder)
+        .then(() => {
+            console.log(
+                `✅ ${projectName} nomli proyekt ${projectFolder} direktoriyasida ochildi!`
+            );
+            // package.json ni yozish
+            return fs.writeFile(
+                path.join(projectFolder, "package.json"),
+                packageJson
+            );
+        })
+        .then(() => {
+            console.log("✅ package.json 📦 yaratildi!");
+            // eslint konfiguratsiya
+            return fs.writeFile(
+                path.join(projectFolder, ".eslintrc"),
+                eslintRc
+            );
+        })
+        .then(() => {
+            console.log("✅ .eslintrc 💫 yaratildi!");
+            return fs.writeFile(
+                path.join(projectFolder, "nodemon.json"),
+                nodemonJson
+            );
+        })
+        .then(() => {
+            console.log("✅ nodemon.json 🟩 yaratildi!");
+            return fs.writeFile(
+                path.join(projectFolder, ".prettierrc"),
+                prettierRc
+            );
+        })
+        .then(() => {
+            console.log("✅ .prettierrc yaratildi!");
+            return fs.writeFile(
+                path.join(projectFolder, ".env"),
+                "# .env da yozgan narsalaringizni .env.example da ham yozib qo'ying!"
+            );
+        })
+        .then(() => {
+            console.log("✅ .env yaratildi!");
+            return fs.writeFile(
+                path.join(projectFolder, ".env.example"),
+                "# .env da yozilgan narsalaringizni ushbu faylga qiymatisiz comment orqali yozib qo'ying masalan: \n" +
+                    "# .env da BOT_TOKEN=yujh789fuiur9409hn bo'lsa bu faylda:\n" +
+                    "# BOT_TOKEN = "
+            );
+        })
+        .then(() => {
+            console.log("✅ .env.example yaratildi!");
+            return fs.writeFile(
+                path.join(projectFolder, "tsconfig.json"),
+                tsConfig
+            );
+        })
+        .then(() => {
+            console.log("✅ tsconfig.json  yaratildi!");
+            return fs.writeFile(
+                path.join(projectFolder, ".gitignore"),
+                gitIgnore
+            );
+        })
+        .then(() => {
+            console.log("✅ .gitignore  yaratildi!");
+            console.log(
+                `\n\n✅ ${projectName} proyekti Initializatsiya qilindi!`
+            );
+        })
+        .catch((err) => {
+            if (err.code === "EEXIST") {
+                console.log(`⚠️ ${projectName} nomli papka allaqachon mavjud!`);
+            } else {
+                console.log(err);
+            }
+        });
+} else {
+    printHelp();
+}
---
+++ utils.js
@@ -0,0 +1,130 @@
+const createJSON = (object) => {
+    return JSON.stringify(object, null, 4);
+};
+
+export const printHelp = () => {
+    console.log(
+        "CLI ni ishlatish uchun ritnyx new [PROJECT_NOMI]ni kiriting!\n"
+    );
+};
+
+export const packageJson = createJSON({
+    name: "telegrafbot-nt",
+    version: "1.0.0",
+    description: "",
+    main: "app.js",
+    type: "module",
+    scripts: {
+        prebuild: "rimraf ./build",
+        dev: "npx nodemon",
+        build: "npm run prebuild && npx tsc",
+        start: "npm run build && node build/main",
+        lint: "npx eslint ./src",
+        format: "npx eslint ./src --fix",
+    },
+    keywords: [],
+    author: "",
+    license: "ISC",
+    devDependencies: {
+        "@types/node": "^18.11.17",
+        "@typescript-eslint/eslint-plugin": "^5.47.0",
+        "@typescript-eslint/parser": "^5.47.0",
+        dotenv: "^16.0.3",
+        eslint: "^8.30.0",
+        husky: "^8.0.2",
+        nodemon: "^2.0.20",
+        prettier: "^2.8.1",
+        rimraf: "^3.0.2",
+        "ts-node": "^10.9.1",
+    },
+});
+
+export const eslintRc = createJSON({
+    semi: true,
+    trailingComma: "all",
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 2,
+});
+
+export const nodemonJson = createJSON({
+    watch: ["src"],
+    ext: ".ts, .js",
+    ignore: [],
+    exec: "npx ts-node ./src/main.ts",
+});
+
+
+export const prettierRc = createJSON({
+    semi: true,
+    trailingComma: "all",
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 2,
+});
+
+export const gitIgnore =
+    "" +
+    `# compiled output
+/dist
+/node_modules
+/build
+.env
+package-lock.json
+
+# Logs
+logs
+*.log
+npm-debug.log*
+pnpm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+lerna-debug.log*
+
+# OS
+.DS_Store
+
+# Tests
+/coverage
+/.nyc_output
+
+# IDEs and editors
+/.idea
+.project
+.classpath
+.c9/
+*.launch
+.settings/
+*.sublime-workspace
+
+# IDE - VSCode
+.vscode/*
+!.vscode/settings.json
+!.vscode/tasks.json
+!.vscode/launch.json
+!.vscode/extensions.json`;
+
+export const tsConfig = createJSON({
+    compilerOptions: {
+        target: "ES2018",
+        experimentalDecorators: true,
+        emitDecoratorMetadata: true,
+        module: "NodeNext",
+        rootDir: "./src",
+        allowJs: true,
+        sourceMap: true,
+        outDir: "./build",
+        removeComments: true,
+        esModuleInterop: true,
+        forceConsistentCasingInFileNames: true,
+        strict: true,
+        noImplicitAny: true,
+        skipLibCheck: true,
+    },
+    include: ["src/**/*"],
+    exclude: ["node_modules"],
+    lib: ["esnext"],
+    "ts-node": {
+        esm: true,
+    },
+});
